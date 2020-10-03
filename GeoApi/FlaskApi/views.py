cca96c5ef7a75920c023160dd2580909c2450c64
Simon Suthers <simon.suthers@pma.co.uk>
2020-10-03 17:43:36 +0100

    Added templates and first formatted version

--- GeoApi/FlaskApi/views.py
+++ GeoApi/FlaskApi/views.py
@@ -9,3 +9,11 @@ def home():
     formatted_now = now.strftime("%A, %d %B, %Y at %X")
 
     return render_template("index.html", title = "Hello Flask", message = "Hello, Flask!", content = " on " + formatted_now)
+
+@app.route('/api/data')
+def get_data():
+    return app.send_static_file('data.json')
+
+@app.route('/about')
+def about():
+    return render_template("about.html", title = "About HelloFlask", content = "Example app page for Flask.")
