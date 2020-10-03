d619da65fe20c08767cb9fd53d739990da58dc86
Simon Suthers <simon.suthers@pma.co.uk>
2020-10-03 17:18:29 +0100

    Added FlaskApi folder

--- GeoApi/app.py
+++ GeoApi/app.py
@@ -2,18 +2,15 @@
 This script runs the application using a development server.
 It contains the definition of routes and views for the application.
 """
+import os
+from FlaskApi import app
 
-from flask import Flask
-app = Flask(__name__)
+#from flask import Flask
+#app = Flask(__name__)
 
 # Make the WSGI interface available at the top level so wfastcgi can get it.
 wsgi_app = app.wsgi_app
 
-
-@app.route('/')
-def hello():
-    """Renders a sample page."""
-    return "Hello World 3!"
 
 if __name__ == '__main__':
     import os
